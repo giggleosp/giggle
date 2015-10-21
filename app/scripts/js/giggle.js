f4f4b28813527efa863253d4a223492c453c1f13
enda <endaphelan1993@gmail.com>
2015-10-21 15:49:16 +0100

    styled navbar and improved effects when opening sidenav

--- app/scripts/js/giggle.js
+++ app/scripts/js/giggle.js
@@ -2,18 +2,38 @@
  * Created by Enda on 20/10/2015.
  */
 
-$(function () {
-  var btnMenu = $("#menu-drawer");
-  var sideNav = $(".side-nav");
-  var wrapper = $(".wrapper");
+  // global controls
+var btnMenu = $("#menu-drawer");
+var menuIcon = $(".ic-menu");
+var sideNav = $(".side-nav");
+var wrapper = $(".wrapper");
 
+$(function () {
+
+  //showSideNav();
+
+  // show/hide side navigation menu
   btnMenu.on("click", function () {
     if(sideNav.css("display") == "none") {
-      sideNav.show("slide", { direction: "right" }, 800);
-      wrapper.removeClass("col-md-12").addClass("col-md-10");
+      showSideNav();
     } else {
-      sideNav.hide("slide", { direction: "right" }, 800);
-      wrapper.removeClass("col-md-10").addClass("col-md-12");
+      hideSideNav();
     }
-  })
+  });
 });
+
+function showSideNav() {
+  wrapper.removeClass("col-md-12").addClass("col-md-10");
+  menuIcon.fadeOut(function () {
+    $(this).text("arrow_forward").fadeIn();
+  });
+  sideNav.show("slide", { direction: "right" }, 400);
+}
+
+function hideSideNav() {
+  wrapper.removeClass("col-md-10").addClass("col-md-12");
+  menuIcon.fadeOut(function () {
+    $(this).text("menu").fadeIn();
+  });
+  sideNav.hide("slide", { direction: "right" }, 400);
+}
