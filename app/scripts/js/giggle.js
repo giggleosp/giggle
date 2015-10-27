ead3096eba8e00dfe18f640d68c5f7de11cc6811
Enda Phelan <Enda Phelan>
2015-10-27 21:12:25 +0000

    touched up colours for active nav links and added dropdown menu for links in side nav

--- app/scripts/js/giggle.js
+++ app/scripts/js/giggle.js
@@ -5,9 +5,16 @@
 // global controls
 var btnMenu = $(".btn-menu");
 var sideNav = $(".side-nav");
+var listItem = $(".nav > li");
+var dropdownListItem = $(".nav > .dropdown > .dropdown-menu > li");
 
 $(function () {
-
+  listItem.on("click", function () {
+    addActiveClassToListItem(this);
+  });
+  dropdownListItem.on("click", function () {
+    addActiveClassToListItem(this);
+  });
   // show/hide side navigation menu
   btnMenu.on("click", function () {
     if(sideNav.css("display") == "none") {
@@ -24,4 +31,26 @@ function showSideNav() {
 
 function hideSideNav() {
   sideNav.hide('slide', {direction: 'left'}, 250);
+}
+
+function addActiveClassToListItem(clicked) {
+  var clickedItem = $(clicked);
+
+  //
+  if(!clickedItem.hasClass("dropdown")) {
+    if(listItem.hasClass("active")) {
+      listItem.removeClass("active");
+    }
+    if(dropdownListItem.hasClass("active")) {
+      dropdownListItem.removeClass("active");
+    }
+    clickedItem.addClass("active");
+  }
+
+  // add active class to parent li if child li is selected
+  var dropdown = clickedItem.closest(".dropdown-menu").closest("li.dropdown");
+  if(dropdown.length > 0) {
+    dropdown.addClass("active");
+  }
+
 }
