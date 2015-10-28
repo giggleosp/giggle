882bd9fbd7d41d3c16f91b7a9c9374d239128d8f
Enda Phelan <Enda Phelan>
2015-10-28 01:10:55 +0000

    refactored sass file to reduce repetition of classes etc. and also split it into multiple imported .scss files for better modularity

--- app/scripts/js/giggle.js
+++ app/scripts/js/giggle.js
@@ -7,6 +7,7 @@ var btnMenu = $(".btn-menu");
 var sideNav = $(".side-nav");
 var listItem = $(".nav > li");
 var dropdownListItem = $(".nav > .dropdown > .dropdown-menu > li");
+var navbarBrand = $(".navbar-brand");
 
 $(function () {
   listItem.on("click", function () {
@@ -23,6 +24,12 @@ $(function () {
       hideSideNav();
     }
   });
+
+  navbarBrand.on("click", function () {
+    removeOtherActiveClassDeclarations();
+    addActiveClassToListItem(".home");
+  })
+
 });
 
 function showSideNav() {
@@ -33,22 +40,24 @@ function hideSideNav() {
   sideNav.hide('slide', {direction: 'left'}, 250);
 }
 
-function addActiveClassToListItem(clicked) {
-  var clickedItem = $(clicked);
+function removeOtherActiveClassDeclarations() {
+  if(listItem.hasClass("active")) {
+    listItem.removeClass("active");
+  }
+  if(dropdownListItem.hasClass("active")) {
+    dropdownListItem.removeClass("active");
+  }
+}
+function addActiveClassToListItem(li) {
+  var item = $(li);
 
-  //
-  if(!clickedItem.hasClass("dropdown")) {
-    if(listItem.hasClass("active")) {
-      listItem.removeClass("active");
-    }
-    if(dropdownListItem.hasClass("active")) {
-      dropdownListItem.removeClass("active");
-    }
-    clickedItem.addClass("active");
+  if(!item.hasClass("dropdown")) {
+    removeOtherActiveClassDeclarations();
+    item.addClass("active");
   }
 
   // add active class to parent li if child li is selected
-  var dropdown = clickedItem.closest(".dropdown-menu").closest("li.dropdown");
+  var dropdown = item.closest(".dropdown-menu").closest("li.dropdown");
   if(dropdown.length > 0) {
     dropdown.addClass("active");
   }
